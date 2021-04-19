029e144eb3bf2c8abcf6f0e218cf1d6c84f72bea
daghankoc21 <dkoc@ucsc.edu>
2021-04-19 08:22:53 -0700

    Added new UI and new theme art

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,7 +15,8 @@ class Play extends Phaser.Scene {
         // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width/2, borderUISize * 2, 0xCD5C5C).setOrigin(0, 0);
+        this.add.rectangle(game.config.width/2, borderUISize + borderPadding, game.config.width/2, borderUISize * 2, 0x344285).setOrigin(0, 0);
         
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
