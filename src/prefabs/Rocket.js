17c742cf4eca03eca3306289abdd94f214cc8f31
daghankoc21 <dkoc@ucsc.edu>
2021-04-19 07:41:35 -0700

    Added two player controls and movement

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,30 +1,56 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
+    constructor(scene, x, y, texture, playernumber) {
+      super(scene, x, y, texture,  playernumber);
 
       this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx  
       // add object to existing scene
       scene.add.existing(this);
       this.isFiring = false;
       this.moveSpeed = 2;
+      this.playernumber = playernumber;
+      
     }
     update() {
+        
         //left or right
-        if(!this.isFiring){
+        if(!this.isFiring && this.playernumber == 1){
             if(keyLEFT.isDown && this.x >= borderUISize + this.width){
                 this.x -= this.moveSpeed;
             } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
                 this.x += this.moveSpeed;
             }
         }
+        if(!this.isFiring && this.playernumber == 2){
+            if(keyA.isDown && this.x >= borderUISize + this.width){
+                this.x -= this.moveSpeed;
+            } else if (keyD.isDown && this.x <= game.config.width - borderUISize - this.width){
+                this.x += this.moveSpeed;
+            }
+        }
         //firing button
-        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring && this.playernumber == 1) {
+            console.log("shot p1");
             this.isFiring = true;
             this.sfxRocket.play(); 
         }
+        if (Phaser.Input.Keyboard.JustDown(keyW)) {
+            if(!this.isFiring){
+                console.log("shot p2");
+                console.log(this.playernumber);
+                if(this.playernumber == 2){
+                    this.isFiring = true;
+                    this.sfxRocket.play(); 
+                }
+            } 
+        }
+       
         //move up
-        if(this.isFiring && this.y >=borderUISize * 3 + borderPadding){
+        if(this.isFiring && this.y >=borderUISize * 3 + borderPadding && this.playernumber == 1){
+            this.y -= this.moveSpeed;
+        }
+        if(this.isFiring && this.y >=borderUISize * 3 + borderPadding && this.playernumber == 2){
+            console.log("p2 shot")
             this.y -= this.moveSpeed;
         }
         //reset on miss
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,14 +23,18 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         // add rocket (p1)
-        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket', 1).setOrigin(0.5, 0);
+        this.p2Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket', 2).setOrigin(0.5, 0);
         // add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         // animation config
@@ -75,7 +79,8 @@ class Play extends Phaser.Scene {
         }
         this.starfield.tilePositionX -= 4;
         if (!this.gameOver) {               
-            this.p1Rocket.update();         
+            this.p1Rocket.update();  
+            this.p2Rocket.update();       
             this.ship01.update();         
             this.ship02.update();
             this.ship03.update();
@@ -93,6 +98,19 @@ class Play extends Phaser.Scene {
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
         }
+        if(this.checkCollision(this.p2Rocket, this.ship03)) {
+            this.p2Rocket.reset();
+            this.shipExplode(this.ship03);   
+        }
+        if (this.checkCollision(this.p2Rocket, this.ship02)) {
+            this.p2Rocket.reset();
+            this.shipExplode(this.ship02);
+        }
+        if (this.checkCollision(this.p2Rocket, this.ship01)) {
+            this.p2Rocket.reset();
+            this.shipExplode(this.ship01);
+        }
+        
     }
     checkCollision(rocket, ship) 
     {
